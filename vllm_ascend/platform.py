0df03aa79c4c50ce7582d3af3644948b37251884
LostFox11 <wangziyue17@huawei.com>
2026-06-28 18:12:33 +0800

    删除校验

--- vllm_ascend/platform.py
+++ vllm_ascend/platform.py
@@ -435,6 +435,8 @@ class NPUPlatform(Platform):
             return
 
         kv_transfer_config = getattr(vllm_config, "kv_transfer_config", None)
+        if kv_transfer_config is None:
+            return
         if kv_transfer_config is not None and getattr(kv_transfer_config, "kv_role", None) == "kv_producer":
             return
 
